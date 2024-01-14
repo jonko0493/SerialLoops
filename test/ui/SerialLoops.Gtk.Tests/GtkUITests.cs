0014a92ed93ec3d21f7ac7715900479d85f2cbd3
jonko0493 <jonko0493@protonmail.com>
2024-01-13 19:48:03 -0800

    Let's do some initial Gtk stuff

--- test/ui/SerialLoops.Gtk.Tests/GtkUITests.cs
+++ test/ui/SerialLoops.Gtk.Tests/GtkUITests.cs
@@ -1,12 +1,74 @@
 using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Remote;
+using SerialLoops.Lib;
+using SerialLoops.Tests.Shared;
+using SerialLoops.UITests.Shared;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
 
 namespace SerialLoops.Gtk.Tests
 {
     public class GtkUITests
     {
-        [SetUp]
+        private RemoteWebDriver? _driver;
+        private UiVals? _uiVals;
+        private Project? _project;
+        private readonly UiTestLogger _logger = new(Environment.GetEnvironmentVariable("LOG_FILE") ?? "testlog_console.log");
+        private readonly ConsoleProgressTracker _tracker = new();
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            if (File.Exists("ui_vals.json"))
+            {
+                _uiVals = JsonSerializer.Deserialize<UiVals>(File.ReadAllText("ui_vals.json")) ?? new();
+                if (!Directory.Exists(_uiVals.ArtifactsDir))
+                {
+                    Directory.CreateDirectory(_uiVals.ArtifactsDir);
+                }
+            }
+            else
+            {
+                string romUri = Environment.GetEnvironmentVariable("ROM_URI") ?? string.Empty;
+                string romPath = Path.Combine(Directory.GetCurrentDirectory(), "HaruhiChokuretsu.nds");
+                HttpClient httpClient = new();
+                using Stream downloadStream = httpClient.Send(new() { Method = HttpMethod.Get, RequestUri = new(romUri) }).Content.ReadAsStream();
+                using FileStream fileStream = new(romPath, FileMode.Create);
+                downloadStream.CopyTo(fileStream);
+                fileStream.Flush();
+
+                _uiVals = new()
+                {
+                    AppLoc = Environment.GetEnvironmentVariable("APP_LOCATION") ?? string.Empty,
+                    ProjectName = Environment.GetEnvironmentVariable("PROJECT_NAME") ?? "MacUITest",
+                    RomLoc = romPath,
+                    ArtifactsDir = Environment.GetEnvironmentVariable("BUILD_ARTIFACTSTAGINGDIRECTORY") ?? "artifacts",
+                };
+            }
+            _logger.Log(JsonSerializer.Serialize(_uiVals, new JsonSerializerOptions { WriteIndented = true }));
+
+            AppiumOptions appiumOptions = new()
+            {
+                PlatformName = "linux",
+            };
+            appiumOptions.AddAdditionalCapability("automationName", "atspi2");
+            appiumOptions.AddAdditionalCapability("appium:appName", _uiVals.AppLoc);
+
+            _driver = new(appiumOptions);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            _driver.GetScreenshot().SaveAsFile(Path.Combine(_uiVals.ArtifactsDir, "start.png"));
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            _driver?.Quit();
         }
 
         [Test]
